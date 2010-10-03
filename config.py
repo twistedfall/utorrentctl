0219c53a11acfc9e8bebfc428c7bb2d8f26568d7
Pavel Procopiuc <hyrurg@gmail.com>
2010-10-03 20:32:28 +0300

    * indicate port in default config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 # default username and password
 utorrentcfg = {
-	'host' : 'default_hostname',
+	'host' : 'default_hostname:1000',
 	'login' : 'default_login',
 	'password' : 'default_password'
 }
